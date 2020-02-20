c58dbf107128e91990a95d9facfcd6f683cc0257
Daniel Olano <danielolanoburga@gmail.com>
2020-02-20 00:08:51 -0500

    lab 4 completed

---
+++ Lab4-Spring-Cloud-Eureka/lab-4-sentence/src/test/java/com/dolano/lab4sentence/Lab4SentenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolano.lab4sentence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4SentenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
